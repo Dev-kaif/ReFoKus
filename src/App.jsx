b8ef88d6bb7439afcbb878e8c4392a93cd34ba6b
Mohammad Kaif <161276501+Dev-kaif@users.noreply.github.com>
2024-12-05 17:47:41 +0530

    made Stripes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from './Components/Navbar.jsx';
 import Work from './Components/Work.jsx';
+import Stripes from './Components/Stripes.jsx'
 
 function App() {
   return (
-    <div className='w-full h-screen font-["Satoshi_variable"] bg-zinc-900 text-white'>
+    <div className='w-full min-h-screen font-["Satoshi_variable"] bg-zinc-900 text-white'>
       <Navbar/>
       <Work/>
+      <Stripes/>
     </div>
   )
 }
