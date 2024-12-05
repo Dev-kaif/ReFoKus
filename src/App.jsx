10fbddf9e57065ed2e52a5edca601cc57c9be500
Mohammad Kaif <161276501+Dev-kaif@users.noreply.github.com>
2024-12-05 16:47:10 +0530

    made hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import Navbar from './Components/Navbar.jsx'
+import Navbar from './Components/Navbar.jsx';
+import Work from './Components/Work.jsx';
 
 function App() {
   return (
     <div className='w-full h-screen font-["Satoshi_variable"] bg-zinc-900 text-white'>
       <Navbar/>
+      <Work/>
     </div>
   )
 }
