8eea6b5119a4e7c8b6a08e6908efb88b4dfe89e8
Mohammad Kaif <161276501+Dev-kaif@users.noreply.github.com>
2024-12-05 18:39:58 +0530

    removed uncessary console log

--- src/Components/Stripe.jsx
+++ src/Components/Stripe.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 function stripe({value,index}) {
-    console.log(index)
     const {url,number} = value;
   return (
     <div className={`w-1/6 px-4 py-5 border-t-[1.2px] border-b-[1.2px] ${index === 5? "":"border-r-[1.2px]"} border-zinc-600 flex justify-between items-center`}>
--- src/Components/Stripes.jsx
+++ src/Components/Stripes.jsx
@@ -1,4 +1,5 @@
-import Stripe from "./Stripe"
+import Stripe from "./Stripe";
+
 function Stripes() {
     const data = [
         {url:"https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63c9ce90a51cd7cf6b3689fb_css-design-awards-logos-id1L9L8Yvp%201.svg",number:68},
@@ -15,4 +16,4 @@ function Stripes() {
   )
 }
 
-export default Stripes
+export default Stripes;
