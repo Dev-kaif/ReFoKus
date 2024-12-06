f8c7104663defa14f6226d97c430293d37d0ea4e
Mohammad Kaif <161276501+Dev-kaif@users.noreply.github.com>
2024-12-06 10:59:58 +0530

    made marquess

--- src/Components/Marqee.jsx
+++ src/Components/Marqee.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-function Marquee() {
+function Marquee({imagesUrl}) {
   return (
-    <div>
-
+    <div className='flex gap-20 w-full overflow-hidden py-10'>
+      {imagesUrl.map((url,i)=><img className='w-[6vw] flex-shrink-0' key={i} src={url} alt="" />)}
+      {imagesUrl.map((url,i)=><img className='w-[6vw] flex-shrink-0' key={i} src={url} alt="" />)}
     </div>
   )
 }
