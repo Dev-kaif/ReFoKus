626034ce8a2094482eb4ef19a9379707c7635bfd
Mohammad Kaif <161276501+Dev-kaif@users.noreply.github.com>
2024-12-06 16:31:03 +0530

    added image animation in hero section usinf framer motion

--- src/Components/Hero/Work.jsx
+++ src/Components/Hero/Work.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import { useMotionValueEvent, useScroll } from "framer-motion";
 
 function Work() {
-  const images = [
+  const [images, setimages] = useState([
     {
       url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef09178195ce0073e38f3_Refokus%20Tools-1.png",
       top: "50%",
@@ -38,7 +39,56 @@ function Work() {
       left: "55%",
       isActive: false,
     },
-  ];
+  ]);
+
+  //The scrollYProgress tracks the vertical scroll progress of the page as a value between 0 and 1.
+  const { scrollYProgress } = useScroll();
+
+  // The useMotionValueEvent hook listens to changes in the scrollYProgress value and updates the images state accordingly.
+  useMotionValueEvent(scrollYProgress, "change", (latest) => {
+
+    const imagesShow = (arr) => {
+      // Update the state of images
+      setimages((prev) =>
+        prev.map((item, index) =>
+          // Check if the current index is in the 'arr' array
+          arr.indexOf(index) === -1
+            ? { ...item, isActive: false } // If not, set 'isActive' to false
+            : { ...item, isActive: true }  // If yes, set 'isActive' to true
+        )
+      );
+    };
+    
+
+    const scrollval = Math.floor(latest * 100);
+    switch (scrollval) {
+      // Scroll progess in the case 
+      case 0: // 0%
+        imagesShow([]);
+        break;
+      case 1: // 1%
+        imagesShow([0]);
+        break;
+      case 2: // 2%
+      // sending the array of indices of the images to show
+        imagesShow([0, 1]);
+        break;
+      case 4:
+        imagesShow([0, 1, 2]);
+        break;
+      case 6:
+        imagesShow([0, 1, 2, 3]);
+        break;
+      case 8:
+        imagesShow([0, 1, 2, 3, 4]);
+        break;
+      case 10:
+        imagesShow([0, 1, 2, 3, 4, 5]);
+        break;
+      default:
+        break;
+    }
+  });
 
   return (
     <div className="w-full mt-10">
