e31a5dcf5c3228bb0eef9cd816417caba2db5ae4
Mohammad Kaif <161276501+Dev-kaif@users.noreply.github.com>
2024-12-06 14:55:51 +0530

    added smooth scroll using lenis

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { ReactLenis, useLenis } from "lenis/react";
+
 import Navbar from "./Components/Navbar/Navbar.jsx";
 import Work from "./Components/Hero/Work.jsx";
 import Stripes from "./Components/Stripes/Stripes.jsx";
@@ -7,16 +9,20 @@ import Cards from "./Components/Card/Cards.jsx";
 import Footer from "./Components/Footer/Footer.jsx";
 
 function App() {
+  const lenis = useLenis();
+
   return (
-    <div className='w-full min-h-screen font-["Satoshi_variable"] bg-zinc-900 text-white'>
-      <Navbar />
-      <Work />
-      <Stripes />
-      <Products />
-      <Marqees />
-      <Cards />
-      <Footer />
-    </div>
+    <ReactLenis root>
+      <div className='w-full min-h-screen font-["Satoshi_variable"] bg-zinc-900 text-white'>
+        <Navbar />
+        <Work />
+        <Stripes />
+        <Products />
+        <Marqees />
+        <Cards />
+        <Footer />
+      </div>
+    </ReactLenis>
   );
 }
 
